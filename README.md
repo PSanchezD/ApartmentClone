35cda818f39d049e37e275adc373a868f8b6baad
pedro sanchez <pedrosanchez707@gmail.com>
2022-06-23 09:36:20 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Built With
+ - React.js
+
---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import Home from '../Page/Home' 
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+function Main (){
+    return(
+        <>
+            <Navbar/>
+            <Home/>
+            <Footer/>
+        </>
+    )
+} 
+export default Main
